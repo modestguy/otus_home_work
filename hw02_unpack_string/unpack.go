f746ef573f755a82b8c032c273fa172af86fd154
PiskunovYv <yurijj.piskunov@vseinstrumenti.ru>
2022-09-13 18:38:58 +0300

    HW2: work with strings, runes, etc

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,59 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(value string) (string, error) {
+	var result strings.Builder
+	var prevCharBuilder strings.Builder
+	var prevNumberCharBuilder strings.Builder
+	var countsBuilder strings.Builder
+	var lastChar rune
+
+	if len(value) == 0 {
+		return "", nil
+	}
+
+	for pos, currentRune := range value {
+		if pos == 0 {
+			if unicode.IsDigit(currentRune) {
+				return "", ErrInvalidString
+			}
+			prevCharBuilder.WriteRune(currentRune)
+			continue
+		}
+
+		if unicode.IsDigit(currentRune) {
+			_, err := strconv.Atoi(prevNumberCharBuilder.String())
+			if err == nil {
+				return "", ErrInvalidString
+			}
+			countsBuilder.WriteRune(currentRune)
+			count, _ := strconv.Atoi(countsBuilder.String())
+			if count != 0 {
+				result.WriteString(strings.Repeat(prevCharBuilder.String(), count))
+			}
+			prevCharBuilder.Reset()
+			prevNumberCharBuilder.WriteRune(currentRune)
+		} else {
+			result.WriteString(prevCharBuilder.String())
+			prevCharBuilder.Reset()
+			prevCharBuilder.WriteRune(currentRune)
+			prevNumberCharBuilder.Reset()
+		}
+		countsBuilder.Reset()
+		lastChar = currentRune
+	}
+
+	if !unicode.IsDigit(lastChar) {
+		prevCharBuilder.Reset()
+		prevCharBuilder.WriteRune(lastChar)
+		result.WriteString(prevCharBuilder.String())
+	}
+	return result.String(), nil
 }
