ec83a5afb39f74cb5b477eb35a17486f70cafdc1
PiskunovYv <yurijj.piskunov@vseinstrumenti.ru>
2022-11-13 12:34:48 +0300

    hw07_file_copying

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,52 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fi, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	size := fi.Size()
+	if size == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > size {
+		return ErrOffsetExceedsFileSize
+	}
+
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(file)
+
+	_, err = file.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+	writeFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer func(writeFile *os.File) {
+		err := writeFile.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(writeFile)
+
+	if limit == 0 {
+		limit = size
+	}
+
+	_, err = io.CopyN(writeFile, file, limit)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,36 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("check file not exists", func(t *testing.T) {
+		err := Copy("/fdgdfgdfg/fdgdfgdfg", "out.txt", 0, 0)
+		require.Truef(t, errors.Is(err, syscall.ENOENT), "actual err - %v", err)
+	})
+
+	t.Run("check cannot get file size", func(t *testing.T) {
+		err := Copy("/dev/urandom", "out.txt", 0, 0)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual err - %v", err)
+	})
+
+	t.Run("check ofsset greater than size", func(t *testing.T) {
+		err := Copy("copy.go", "out.txt", 1000000000000, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual err - %v", err)
+	})
+
+	t.Run("check create file error", func(t *testing.T) {
+		err := Copy("./testdata/input.txt", "/cdrom/test.txt", 0, 0)
+		require.Truef(t, errors.Is(err, syscall.EACCES), "actual err - %v", err)
+	})
+
+	t.Run("check seek error", func(t *testing.T) {
+		err := Copy("/dev", "out.txt", 10, 0)
+		require.Truef(t, errors.Is(err, syscall.EISDIR), "actual err - %v", err)
+	})
 }
